9885fdfd2dcc5a4897b468644ae290fa1450faa1
LucasPmep <lucas.gillet@pme-partner.fr>
2023-04-19 12:13:26 +0200

    premières implémentations : bdd models controllers

---
+++ example-app/app/Http/Controllers/Api/PersonController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Person;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PersonResource;
+
+class PersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return PersonResource::collection(Person::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Person $person)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Person $person)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Person $person)
+    {
+        //
+    }
+}
